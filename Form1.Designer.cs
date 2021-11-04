33b8687e43ae8b6657039d67f2b958430731c819
Yigu Wang <47022281+POPCORNBOOM@users.noreply.github.com>
2021-11-04 18:55:01 +0800

    main files
    upload main files

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace NetDrawLots
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dgv_from = new System.Windows.Forms.DataGridView();
+            this.names = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_roll = new System.Windows.Forms.Button();
+            this.tb_count = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_allowrepeat = new System.Windows.Forms.CheckBox();
+            this.btn_import = new System.Windows.Forms.Button();
+            this.dgv_output = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_10 = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_log = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_log = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_showcode = new System.Windows.Forms.Button();
+            this.btn_clearlog = new System.Windows.Forms.Button();
+            this.btn_opensite = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_from)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_output)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgv_from
+            // 
+            this.dgv_from.AllowUserToAddRows = false;
+            this.dgv_from.AllowUserToDeleteRows = false;
+            this.dgv_from.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_from.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.names});
+            resources.ApplyResources(this.dgv_from, "dgv_from");
+            this.dgv_from.MultiSelect = false;
+            this.dgv_from.Name = "dgv_from";
+            this.dgv_from.ReadOnly = true;
+            this.dgv_from.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dgv_from.RowTemplate.Height = 27;
+            this.dgv_from.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            // 
+            // names
+            // 
+            resources.ApplyResources(this.names, "names");
+            this.names.Name = "names";
+            this.names.ReadOnly = true;
+            // 
+            // btn_roll
+            // 
+            resources.ApplyResources(this.btn_roll, "btn_roll");
+            this.btn_roll.BackColor = System.Drawing.Color.RosyBrown;
+            this.btn_roll.FlatAppearance.BorderSize = 0;
+            this.btn_roll.Name = "btn_roll";
+            this.btn_roll.UseVisualStyleBackColor = false;
+            this.btn_roll.Click += new System.EventHandler(this.btn_roll_Click);
+            // 
+            // tb_count
+            // 
+            resources.ApplyResources(this.tb_count, "tb_count");
+            this.tb_count.Name = "tb_count";
+            this.tb_count.Leave += new System.EventHandler(this.tb_count_Leave);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // cb_allowrepeat
+            // 
+            resources.ApplyResources(this.cb_allowrepeat, "cb_allowrepeat");
+            this.cb_allowrepeat.Name = "cb_allowrepeat";
+            this.cb_allowrepeat.UseVisualStyleBackColor = true;
+            // 
+            // btn_import
+            // 
+            resources.ApplyResources(this.btn_import, "btn_import");
+            this.btn_import.Name = "btn_import";
+            this.btn_import.UseVisualStyleBackColor = true;
+            this.btn_import.Click += new System.EventHandler(this.btn_import_Click);
+            // 
+            // dgv_output
+            // 
+            this.dgv_output.AllowUserToAddRows = false;
+            this.dgv_output.AllowUserToDeleteRows = false;
+            this.dgv_output.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_output.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1});
+            resources.ApplyResources(this.dgv_output, "dgv_output");
+            this.dgv_output.MultiSelect = false;
+            this.dgv_output.Name = "dgv_output";
+            this.dgv_output.ReadOnly = true;
+            this.dgv_output.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dgv_output.RowTemplate.Height = 27;
+            this.dgv_output.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            resources.ApplyResources(this.dataGridViewTextBoxColumn1, "dataGridViewTextBoxColumn1");
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Controls.Add(this.btn_roll);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cb_allowrepeat);
+            this.groupBox1.Controls.Add(this.tb_count);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_10);
+            this.groupBox2.Controls.Add(this.btn_1);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // btn_10
+            // 
+            resources.ApplyResources(this.btn_10, "btn_10");
+            this.btn_10.BackColor = System.Drawing.Color.Gold;
+            this.btn_10.FlatAppearance.BorderSize = 0;
+            this.btn_10.ForeColor = System.Drawing.Color.White;
+            this.btn_10.Name = "btn_10";
+            this.btn_10.UseVisualStyleBackColor = false;
+            this.btn_10.Click += new System.EventHandler(this.btn_10_Click);
+            // 
+            // btn_1
+            // 
+            this.btn_1.BackColor = System.Drawing.Color.Cornsilk;
+            this.btn_1.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btn_1, "btn_1");
+            this.btn_1.Name = "btn_1";
+            this.btn_1.UseVisualStyleBackColor = false;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_log
+            // 
+            resources.ApplyResources(this.btn_log, "btn_log");
+            this.btn_log.Name = "btn_log";
+            this.btn_log.UseVisualStyleBackColor = true;
+            this.btn_log.Click += new System.EventHandler(this.btn_log_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dgv_from);
+            this.groupBox3.Controls.Add(this.btn_import);
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.dgv_output);
+            resources.ApplyResources(this.groupBox4, "groupBox4");
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.TabStop = false;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tb_log
+            // 
+            resources.ApplyResources(this.tb_log, "tb_log");
+            this.tb_log.Name = "tb_log";
+            this.tb_log.ReadOnly = true;
+            // 
+            // groupBox5
+            // 
+            resources.ApplyResources(this.groupBox5, "groupBox5");
+            this.groupBox5.Controls.Add(this.btn_opensite);
+            this.groupBox5.Controls.Add(this.btn_showcode);
+            this.groupBox5.Controls.Add(this.tb_log);
+            this.groupBox5.Controls.Add(this.btn_clearlog);
+            this.groupBox5.Controls.Add(this.btn_log);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.TabStop = false;
+            // 
+            // groupBox6
+            // 
+            resources.ApplyResources(this.groupBox6, "groupBox6");
+            this.groupBox6.Controls.Add(this.groupBox2);
+            this.groupBox6.Controls.Add(this.groupBox5);
+            this.groupBox6.Controls.Add(this.groupBox1);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.groupBox3);
+            this.panel2.Controls.Add(this.groupBox6);
+            this.panel2.Controls.Add(this.groupBox4);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // btn_showcode
+            // 
+            resources.ApplyResources(this.btn_showcode, "btn_showcode");
+            this.btn_showcode.Name = "btn_showcode";
+            this.btn_showcode.UseVisualStyleBackColor = true;
+            this.btn_showcode.Click += new System.EventHandler(this.btn_showcode_Click);
+            // 
+            // btn_clearlog
+            // 
+            resources.ApplyResources(this.btn_clearlog, "btn_clearlog");
+            this.btn_clearlog.Name = "btn_clearlog";
+            this.btn_clearlog.UseVisualStyleBackColor = true;
+            this.btn_clearlog.Click += new System.EventHandler(this.btn_clearlog_Click);
+            // 
+            // btn_opensite
+            // 
+            resources.ApplyResources(this.btn_opensite, "btn_opensite");
+            this.btn_opensite.Name = "btn_opensite";
+            this.btn_opensite.UseVisualStyleBackColor = true;
+            this.btn_opensite.Click += new System.EventHandler(this.btn_opensite_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_from)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_output)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_from;
+        private System.Windows.Forms.Button btn_roll;
+        private System.Windows.Forms.TextBox tb_count;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cb_allowrepeat;
+        private System.Windows.Forms.Button btn_import;
+        private System.Windows.Forms.DataGridViewTextBoxColumn names;
+        private System.Windows.Forms.DataGridView dgv_output;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_10;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_log;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_log;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_showcode;
+        private System.Windows.Forms.Button btn_clearlog;
+        private System.Windows.Forms.Button btn_opensite;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace NetDrawLots
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "NetDrawLots   v" + Application.ProductVersion;
+            if (File.Exists(Properties.Settings.Default.lastopen))
+                try
+                {
+                    logtofile("Try Open Last File");
+                    openafile(Properties.Settings.Default.lastopen);
+                }
+                catch (Exception ex)
+                {
+                    logtofile("Error:Failed to Open Last File.Details(" + ex + ")");
+
+                }
+            else
+                Properties.Settings.Default.lastopen = null;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btn_import_Click(object sender, EventArgs e)
+        {
+            logtofile("OpenFileDialog dialog = new OpenFileDialog()");
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;//允许打开多个文件
+            dialog.Title = "请选择文件";
+            dialog.Filter = "逗号分隔文件|*.csv|All|*.*";//打开多个文件
+            string filePath;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+                filePath = dialog.FileName;
+            else
+            {
+                logtofile("Cancel");
+                return;
+            }
+            openafile(filePath);
+        }
+
+        public void openafile(string filePath)
+        {
+            logtofile("Pool Import from Path:" + filePath);
+            Properties.Settings.Default.lastopen = filePath;
+            Properties.Settings.Default.Save();
+            dgv_from.Rows.Clear();
+            dgv_from.Refresh();
+            int index = 0;
+            foreach (string str in File.ReadAllLines(filePath, Encoding.Default))
+            {
+                if (str != null)
+                {
+                    dgv_from.Rows.Add(str);
+                    //dgv_from.Rows[index].HeaderCell.Value = index + 1;
+                }
+                index++;
+
+            }
+            if (index >= 11)
+                btn_10.Enabled = true;
+            else
+                btn_10.Enabled = false;
+
+        }
+
+        private void dgv_from_RowStateChanged(object sender, DataGridViewRowStateChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_roll_Click(object sender, EventArgs e)
+        {
+            roll(int.Parse(tb_count.Text));
+        }
+
+        public void roll(int rollcount)
+        {
+            string rollid = ((int)DateTime.Now.Ticks).ToString("X");
+            logtofile("Start New Roll[" + rollid + "] with RollCount: " + rollcount);
+            List<string> result = new List<string>();
+            List<string> names = new List<string>();
+            if (dgv_from.Rows.Count == 0)
+            {
+                logtofile("Error:Null Pool");
+                logtofile("End Roll[" + rollid + "]");
+                return;
+
+            }
+            int rr = 0;
+            string temp=string.Empty;
+            foreach (DataGridViewRow row in dgv_from.Rows)
+            {
+                names.Add(row.Cells[0].Value.ToString());
+                temp += row.Cells[0].Value.ToString()+",";
+            }
+            logtofile("Load Pool:[ " + temp+"]");
+
+            while (result.Count < rollcount)
+            {
+                Random rdrd = new Random((int)DateTime.Now.Ticks);
+                int newrdrd = (int)(rdrd.NextDouble() * (int)DateTime.Now.Ticks);
+                Random rd = new Random(newrdrd + rr);
+                //Random rd = new Random((int)DateTime.Now.Ticks+rr);
+                logtofile("New Random with Seed: " + (newrdrd+rr));
+                int a = (int)(rd.NextDouble() * names.Count);
+                logtofile("New Random Result: " + a + " refer to" + names[a]);
+                result.Add(names[a]);
+                //if (dgv_from.Rows[a].HeaderCell.Value == null) dgv_from.Rows[a].HeaderCell.Value = 0;
+                //dgv_from.Rows[a].HeaderCell.Value = (int)dgv_from.Rows[a].HeaderCell.Value + 1;
+                if (!cb_allowrepeat.Checked)
+                    names.RemoveAt(a);
+                rr += 1;
+            }
+
+            dgv_output.Rows.Clear();
+            dgv_output.Refresh();
+            foreach (string b in result)
+            {
+                dgv_output.Rows.Add(b);
+            }
+            dgv_output.Refresh();
+            logtofile("End Roll[" + rollid + "]");
+        }
+
+
+        public static bool IsNumber(string s, int precision, int scale)
+        {
+            int a;
+            try
+            {
+                a = int.Parse(s);
+            }
+            catch
+            {
+                return false;
+            }
+            if (a < precision + 1 && a > scale - 1)
+                return true;
+            else
+                return false;
+        }
+
+        private void tb_count_Leave(object sender, EventArgs e)
+        {
+            if (!IsNumber(tb_count.Text, dgv_from.Rows.Count, 0))
+            {
+                tb_count.Text = "1";
+                MessageBox.Show("{count|count∈N*,rowcount>=count>0}");
+            }
+        }
+
+        private void btn_10_Click(object sender, EventArgs e)
+        {
+            roll(10);
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            roll(1);
+        }
+
+        public void logtofile(string logstr)
+        {
+            string dir = Properties.Settings.Default.logdir+ "lotlog.txt";
+            if (!File.Exists(dir))
+                File.Create(dir);
+            //FileStream fs = new FileStream(dir, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(dir, true, Encoding.Default);
+            sw.WriteLine(DateTime.Now + " | " + logstr);
+            sw.Flush();
+            sw.Close();
+            tb_log.Text += (DateTime.Now + " | " + logstr + "\r\n");
+            //fs.Close();
+        }
+
+        private void btn_log_Click(object sender, EventArgs e)
+        {
+            string dir = Properties.Settings.Default.logdir+ "lotlog.txt";
+            if (!File.Exists(dir))
+                File.Create(dir);
+            Process.Start(dir);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://popcornboom.github.io/");
+        }
+
+        private void btn_showcode_Click(object sender, EventArgs e)
+        {
+            string tempcode = Properties.Settings.Default.logdir + "code.png";
+            if (!File.Exists(tempcode))
+            {
+                Properties.Resources.sourcecode.Save(tempcode);
+            }
+            Process.Start(tempcode);
+
+        }
+
+        private void btn_clearlog_Click(object sender, EventArgs e)
+        {
+            tb_log.Text = string.Empty;
+        }
+
+        private void btn_opensite_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/POPCORNBOOM/NetDrawLots");
+        }
+    }
+}
